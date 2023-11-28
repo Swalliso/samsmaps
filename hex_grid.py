e67dd3533dbda3f42d68a1859cb108be0d0b83e4
Sam Allison <Swalliso09@gmail.com>
2023-11-27 20:52:14 -0600

    optimized neighbor calculation & error handling

--- hex_grid.py
+++ hex_grid.py
@@ -3,16 +3,8 @@ from hex_tile import HexTile
 
 class HexGrid:
 
-    neighbor_offsets = [
-            (-1, 0),
-            (1, 0),
-            (0, -1),
-            (0, 1),
-            (-1, 1),
-            (1, -1),
-        ]
-
-    terrain_probabilities = {
+    # Default probabilities, features, and resources for each terrain type
+    DEFAULT_TERRAIN_PROBABILITIES = {
         'grass': 0.3,
         'plains': 0.2,
         'desert': 0.1,
@@ -21,7 +13,7 @@ class HexGrid:
         'water': 0.2,
     }
 
-    terrain_features = {
+    DEFAULT_TERRAIN_FEATURES = {
         'grass': ['woods', 'rainforests'],
         'plains': ['woods'],
         'desert': ['oasis'],
@@ -30,7 +22,7 @@ class HexGrid:
         'water': ['reef'],
     }
 
-    terrain_resources = {
+    DEFAULT_TERRAIN_RESOURCES = {
         'grass': ['bananas', 'deer', 'ivory', 'spices', 'sugar', 'truffles'],
         'plains': ['copper', 'maize'],
         'desert': ['gems', 'gold'],
@@ -39,25 +31,47 @@ class HexGrid:
         'water': ['fish', 'pearls', 'whales']
     }
 
-    def __init__(self, rows, cols, default_terrain):
+    # Offsets to find neighboring tiles in a hex grid
+    NEIGHBOR_OFFSETS = [
+        # Coordinates relative to a tile to find its neighbors
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1),
+        (-1, 1),
+        (1, -1),
+    ]
+
+    def __init__(self, rows, cols, default_terrain, terrain_probabilities=None, terrain_features=None, terrain_resources=None):
+        # Initialization of the HexGrid object
+        # Validates input for grid dimensions
+        if rows <= 0 or cols <= 0:
+            raise ValueError("Rows and columns must be positive integers")
+
+        # Setting grid dimensions and initializing grid with default terrain
         self.rows = rows
         self.cols = cols
         self.grid = [[HexTile(default_terrain) for _ in range(cols)] for _ in range(rows)]
 
+        # Set default terrain probabilities, features, and resources if not provided
+        self.terrain_probabilities = terrain_probabilities or self.DEFAULT_TERRAIN_PROBABILITIES
+        self.terrain_features = terrain_features or self.DEFAULT_TERRAIN_FEATURES
+        self.terrain_resources = terrain_resources or self.DEFAULT_TERRAIN_RESOURCES
+
+    def get_neighbor_coords(self, row, col):
+        # Generate coordinates of neighboring tiles
+        for dr, dc in self.NEIGHBOR_OFFSETS:
+            r, c = row + dr, col + dc
+            if 0 <= r < self.rows and 0 <= c < self.cols:
+                yield r, c
+
     def get_neighbor_types(self, row, col):
-        neighbor_coords = [
-            (row - 1, col),
-            (row + 1, col),
-            (row, col - 1),
-            (row, col + 1),
-            (row - 1, col + 1 if row % 2 == 0 else col - 1),
-            (row + 1, col + 1 if row % 2 == 0 else col - 1),
-        ]
-        return [self.grid[r][c].terrain_type for r, c in neighbor_coords
-                if 0 <= r < self.rows and 0 <= c < self.cols]
+        # Retrieve the terrain types of neighboring tiles
+        return [self.grid[r][c].terrain_type for r, c in self.get_neighbor_coords(row, col)]
 
     def choose_terrain_type(self, row, col):
-        # Probabilities of a terrain type given its neighboring types
+        # Determine the terrain type of a tile based on its neighbors
+        # Define transition probabilities for terrain types based on neighbors
         transition_probabilities = {
             'grass': {'plains': 0.4, 'desert': 0.1, 'grass': 0.5},
             'plains': {'grass': 0.4, 'desert': 0.2, 'plains': 0.4},
@@ -75,44 +89,36 @@ class HexGrid:
             for terrain, prob in transition_probabilities[neighbor].items():
                 terrain_probabilities[terrain] += prob
 
-        # Normalize probabilities
+        # Normalize probabilities to ensure they sum to 1
         total_prob = sum(terrain_probabilities.values())
         for terrain in terrain_probabilities:
             terrain_probabilities[terrain] /= total_prob
 
+        # Randomly choose a terrain type based on the calculated probabilities
         terrain_types, probabilities = zip(*terrain_probabilities.items())
         return random.choices(terrain_types, probabilities, k=1)[0]
 
     def choose_terrain_feature(self, terrain_type):
-        if random.random() < 0.5:  # 50% chance of having a feature
+        # Randomly decide if a terrain feature should be added (50% chance)
+        if random.random() < 0.5:
             features = self.terrain_features.get(terrain_type, [])
             return random.choice(features) if features else None
         else:
             return None
 
     def choose_resource(self, terrain_type, terrain_feature):
-        if random.random() < 0.3:  # 30% chance of having a resource
+        # Randomly decide if a resource should be added (30% chance)
+        if random.random() < 0.3:
             resources = self.terrain_resources.get(terrain_type, []) + self.terrain_resources.get(terrain_feature, [])
             return random.choice(resources) if resources else None
         else:
             return None
 
     def generate_terrain(self):
+        # Generate the terrain for each tile in the grid
         for row in range(self.rows):
             for col in range(self.cols):
                 tile = self.grid[row][col]
-                
-                # determine the terrain type for this tile
-                # (this is where we would add logic to make certain terrains more likely 
-                # to be next to each other, etc.)
                 tile.terrain_type = self.choose_terrain_type(row, col)
-                
-                # determine the terrain feature for this tile
-                # (this is where we would add logic to make certain features more likely
-                # on certain terrains, etc.)
                 tile.terrain_feature = self.choose_terrain_feature(tile.terrain_type)
-                
-                # determine the resource for this tile
-                # (this is where we would add logic to make certain resources more likely
-                # on certain features, etc.)
                 tile.resource = self.choose_resource(tile.terrain_type, tile.terrain_feature)
